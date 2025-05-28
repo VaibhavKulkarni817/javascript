fbfae004304d6663494b4b8294d0bd94c73165e0
Vaibhav Kulkarni <142078742+VaibhavKulkarni817@users.noreply.github.com>
2025-05-28 12:30:46 +0000

    variable declare

---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "vaibhav643@gmail.com"
+var accountPassword = "1234"
+accountCity = "Pune"
+let accountState;
+
+
+// accountId = 2 // not allowed
+
+accountEmail = "abc@gmail.com"
+accountPassword = "233456"
+accountCity = "Satara"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
